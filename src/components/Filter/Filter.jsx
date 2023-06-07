d6be7b71ba4394f81dea9b5930a5e3038c1ee714
bosvill <sv.boecker@gmail.com>
2023-06-07 11:50:28 +0200

    final commit

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,9 +3,9 @@ import { useParams } from 'react-router-dom'
 import styles from './Filter.module.css'
 import { useDisplayStoresQuery } from '../../features/store/storeApi'
 import { useDisplayBrandQuery } from '../../features/brand/brandApi'
-import {useDisplayStoreProductsQuery} from '../../features/product/productApi'
+import { useDisplayStoreProductsQuery } from '../../features/product/productApi'
 import { useDisplayCategoriesQuery } from '../../features/category/categoryApi'
-import { useDisplayCategoryQuery} from '../../features/category/categoryApi'
+import { useDisplayCategoryQuery } from '../../features/category/categoryApi'
 import { useDisplaySubcategoriesQuery } from '../../features/subcategory/subcategoryApi'
 import ProductCard from '../ProductCard/ProductCard'
 
@@ -29,36 +29,35 @@ const Filter = () => {
 		limit: categoryLimit */
 	})
 
-	const {data:productsData, isLoading:isProductsLoading}= useDisplayStoreProductsQuery(store)
+	const { data: productsData, isLoading: isProductsLoading } = useDisplayStoreProductsQuery(store)
 	useEffect(() => {
 		setExplore(filter)
 		console.log(explore)
 	}, [filter])
 
 	const brand = brandData?.data || []
-	const products = productsData?.data
+	const products = productsData?.data || []
 	const filteredProducts = products.filter(el => el.brand?._id === fid)
-	console.log('brand:',brand)
-	
+	console.log('brand:', brand)
+
 	console.log(filter)
 	const categories = categoriesData?.data || []
-	console.log('categories:',categories)
+	console.log('categories:', categories)
 	const category = categoryData?.data || []
-	console.log('category:',category)
+	console.log('category:', category)
 	const subcategories = subcategoriesData?.data || []
-	console.log('subcategories:',subcategories)
+	console.log('subcategories:', subcategories)
 	return (
 		<section className={styles.filter}>
 			<header className={styles.filterHeader}>{brand.title}</header>
 			<div className={styles.filterWrapper}>
-
-			<a href={brand.website} target='_blank' rel='noopener noreferrer'>
-				<img src={brand.logo?.url} alt={brand.title} className={styles.filterImg} />
-			</a>
-			<p className={styles.description}> {brand.description}</p>
+				<a href={brand.website} target='_blank' rel='noopener noreferrer'>
+					<img src={brand.logo?.url} alt={brand.title} className={styles.filterImg} />
+				</a>
+				<p className={styles.description}> {brand.description}</p>
 			</div>
 			<section className={styles.grid}>
-{filteredProducts.map(el=><ProductCard key={el.id} product={el}/>)}
+				{filteredProducts && filteredProducts.map(el => <ProductCard key={el.id} product={el} />)}
 			</section>
 		</section>
 	)
